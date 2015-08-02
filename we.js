20cfd3099cd2e34654fa39d24c5a401a02c399e3
吉星 <shihong.xsh@alibaba-inc.com>
2015-08-02 14:39:44 +0800

    examples

--- we.js
+++ we.js
@@ -1,20 +1,16 @@
 (function() {
     var script = document.getElementById('wejs');
-    var src = script.src.replace('we.js', '');
-    // var widthLib=script
-
+    var path = script.src.replace('we.js', '');
 
     var scripts = [
-        '<link type="text/css" rel="stylesheet" href="', css, '/>'
+        '<link type="text/css" rel="stylesheet" href="', path, 'css/we.css"/>'
     ];
 
 
     var srcList = ['core', 'component', 'ajax', 'auto-complete', 'date-picker', 'dialog', 'form', 'grid', 'mask', 'message', 'number-editor', 'pagebar', 'select', 'tabs'];
 
     for (var i = 0, len = srcList.length; i < len; i++) {
-        scripts = scripts.concat(['<script type="text/javascript" src="'
-            src, '/js/', srcList[i], '.js"><\\/', , 'script>'
-        ]);
+        scripts = scripts.concat(['<script type="text/javascript" src="', path, 'js/', srcList[i], '.js"><\/', 'script>']);
     }
 
     scripts = scripts.join('');
