709a7c716b170aca6ba444a667fe06cf6763c74d
吉星 <shihong.xsh@alibaba-inc.com>
2015-08-20 21:39:26 +0800

    修改wejs loader

--- we.js
+++ we.js
@@ -8,8 +8,17 @@
         '<link type="text/css" rel="stylesheet" href="', path, 'css/we.css"/>'
     ];
 
+    var loaders = $(script).data('loader');
 
-    var srcList = ['core', 'component', 'ajax', 'auto-complete', 'date-picker', 'dialog', 'form', 'grid', 'mask', 'message', 'number-editor', 'pagebar', 'select', 'tabs'];
+    var srcList = [];
+
+    if (loaders == 'all') {
+        srcList = ['core', 'component', 'ajax', 'auto-complete', 'date-picker', 'dialog', 'form', 'grid', 'mask', 'message', 'number-editor', 'pagebar', 'select', 'tabs'];
+    } else if (!loaders) {
+
+    } else {
+        srcList = loaders.split(',');
+    }
 
     for (var i = 0, len = srcList.length; i < len; i++) {
         scripts = scripts.concat(['<script type="text/javascript" src="', path, 'js/', srcList[i], '.js"><\/', 'script>']);
